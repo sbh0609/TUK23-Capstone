d57ec05f3fe2f1f76c28da45446a48096f4a7ef0
OneTable146 <onetable146@gmail.com>
2024-05-12 21:39:58 +0900

    임시저장 2

---
+++ front/src/tests/dbtest.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/users')
+            .then(response => response.json())
+            .then(data => setUsers(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Users</h1>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.username} - {user.email}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
